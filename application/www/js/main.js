8b540049b73f7bcd8f7b7c869b000b36b561617d
unknown <thiebautp@gmail.com>
2019-12-21 16:29:58 +0100

    Page d'acceuil site AS

---
+++ application/www/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function onMouseOverDisplayMenu(event) {
+  event.preventDefault();
+  let name = $(this).data("name");
+  console.log(name);
+  $(".sub-nav-" + name).removeClass("hide");
+}
+
+function onMouseOutHideMenu(event) {
+  event.preventDefault();
+  $(".sub-nav").addClass("hide");
+}
+
+$(".nav-item").on("mouseover", onMouseOverDisplayMenu);
+$(".nav-item").on("mouseout", onMouseOutHideMenu);
+$(".navButton").on("mouseout", onMouseOutHideMenu);
